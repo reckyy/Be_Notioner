87830e6fa0e7d4f68be9254cb546206dc0e644ca
recky <srreki98@gmail.com>
2023-04-02 12:16:18 +0900

    タイムゾーンの設定完了 #2

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,10 @@ module BeNotioner
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    #タイムゾーンの設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local #DBのタイムゾーンを変更
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
