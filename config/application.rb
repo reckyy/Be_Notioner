bc5a57bfe259bae38db89c7a02a1376d9383b193
recky <srreki98@gmail.com>
2023-04-02 16:33:53 +0900

    application.rbに追加

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,9 @@ module BeNotioner
     #タイムゾーンの設定
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local #DBのタイムゾーンを変更
+    #18n対応
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Configuration for the application, engines, and railties goes here.
     #
