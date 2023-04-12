0ce6e460026c0c10a7dc565e61725308719468d8
recky <srreki98@gmail.com>
2023-04-12 10:16:00 +0900

    LoginSupportをinclude#44

--- spec/support/login_support.rb
+++ spec/support/login_support.rb
@@ -1,9 +1,9 @@
 module LoginSupport
   def login(user)
     visit root_path
-    click_link "Login"
+    click_link "ログイン"
     fill_in "メールアドレス", with: user.email
     fill_in "パスワード", with: user.password
-    click_button "Login"
+    click_button "ログイン"
   end
 end
