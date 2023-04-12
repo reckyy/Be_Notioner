254d566e1ce0f6a4246168b3745ae2cab78eb79f
recky <srreki98@gmail.com>
2023-04-12 12:24:30 +0900

    ログインログアウト時のflashをi18n対応#44

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,15 +4,15 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path, success:
+      redirect_back_or_to root_path, success: (t '.success')
     else
-      flash.now[:danger] = ''
+      flash.now[:danger] = (t '.danger')
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     logout
-    redirect_to root_path, status: :see_other, success: ''
+    redirect_to root_path, status: :see_other, success: (t '.success')
   end
 end
