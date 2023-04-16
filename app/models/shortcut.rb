6fa03fffc918b0c5ee6cc219742d308c8f4cd1a1
recky <srreki98@gmail.com>
2023-04-16 17:14:09 +0900

    shortcut.keysにlengthのvalidation追加#58

--- app/models/shortcut.rb
+++ app/models/shortcut.rb
@@ -1,4 +1,4 @@
 class Shortcut < ApplicationRecord
   validates :title, presence: true
-  validates :keys, presence: true, uniqueness: true
+  validates :keys, presence: true, uniqueness: true, length: { maximum: 10 }
 end
