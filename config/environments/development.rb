7cc5c74fe2705341a16d9dc063351fff03d9f04b
recky <srreki98@gmail.com>
2023-05-04 11:03:00 +0900

    mailer関連の記述をconfigに追加#22

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
+  config.action_mailer.delivery_method = :letter_opener_web
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
