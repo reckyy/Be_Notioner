cb5ac7a0907148ee6568e4ab7fa3a27f8136dd51
recky <srreki98@gmail.com>
2023-05-04 10:04:13 +0900

    Letter_Opener_Webにアクセスするために必要な記述を追加#22

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create]
   resources :shortcuts, only: %i[index]
   resources :password_resets, only: %i[new create edit update]
+
+  #letter_opener_webにアクセスするために
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
