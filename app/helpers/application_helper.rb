22af9918603ef7ffc8810a9aa0538f106b204ebf
recky <srreki98@gmail.com>
2023-06-07 16:48:59 +0900

    default_meta_tagsのsyntaxerrorを解消#85

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,22 +2,20 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'Be Notioner',
-      title: 'Notionとともに1日を生きよう。'
+      title: 'Notionとともに1日を生きよう。',
       reverse: 'false',
       description: 'Be Notionerは、「Notionを使ってみたいけど何からしたらいいかわからない」方にNotionのテンプレートなどを提供するサービスです。',
       keywords: '効率化,Notion,タスク,テンプレート,プログラミング,インプット',
       canonical: request.original_url,
       separator: '|',
-      icon: [
-
-      ],
+      #icon: [],
       og: {
         site_name: :site,
         title: :full_title,#:full_titleはサイトに表示される<title>と同一のものを表示
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url(),
+        #image: image_url(),
         locale: 'ja_JP',
       },
       twitter: {
