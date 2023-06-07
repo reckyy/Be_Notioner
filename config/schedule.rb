17c0f3f563937a82cbf8fe062a7313417095efb5
recky <srreki98@gmail.com>
2023-06-07 18:01:59 +0900

    毎朝8時にsitemapの更新を行う内容を記述#87

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,8 +13,8 @@
 #   rake "some:great:rake:task"
 # end
 #
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+every 1.day, at: '8:00 am' do
+  rails 'sitemap:refresh'
+end
 
 # Learn more: http://github.com/javan/whenever
