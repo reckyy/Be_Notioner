4856814da5129d45f5ad7bede47215c2a121318b
recky <srreki98@gmail.com>
2023-06-08 17:38:50 +0900

    画像関連の記述を追加#75

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,14 +8,17 @@ module ApplicationHelper
       keywords: '効率化,Notion,タスク,テンプレート,プログラミング,インプット',
       canonical: request.original_url,
       separator: '|',
-      #icon: [],
+      icon: [
+        { herf: image_url('favicon.ico') },
+        { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
       og: {
         site_name: :site,
         title: :full_title,#:full_titleはサイトに表示される<title>と同一のものを表示
         description: :description,
         type: 'website',
         url: request.original_url,
-        #image: image_url(),
+        image: image_url('ogp.png'),
         locale: 'ja_JP',
       },
       twitter: {
