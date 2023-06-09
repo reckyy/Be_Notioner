32c936fadb350dccbac0078cabb764f62b182d57
recky <srreki98@gmail.com>
2023-06-09 12:55:50 +0900

    sitemapへのルーティング追加ルーティング追加 #87

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,7 @@ Rails.application.routes.draw do
 
   #letter_opener_webにアクセスするために
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
+  #sitemapへのルーティング
+  get '/sitemap', to: redirect("https://s3-ap-northeast-1.amazonaws.com/#{ENV['S3_BUCKET_NAME']}/sitemaps/sitemap.xml.gz")
 end
