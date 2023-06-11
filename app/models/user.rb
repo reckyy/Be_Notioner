f661bead3a31176bc4abed8cd47b0e199c9594ca
recky <srreki98@gmail.com>
2023-06-11 20:09:41 +0900

    Userモデルにlogin_type関連を追加#107

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 3 }, if: -> { sorcery? && (new_record? || changes[:crypted_password]) }
+  validates :password, confirmation: true, if: -> { sorcery? && (new_record? || changes[:crypted_password]) }
+  validates :password_confirmation, presence: true, if: -> { sorcery? && (new_record? || changes[:crypted_password]) }
   validates :reset_password_token, presence: true, uniqueness: true, allow_nil: true
 
-  validates :email, uniqueness: true, presence: true
+  validates :email, uniqueness: { scope: :login_type }, presence: true
   validates :name, presence: true, length: { maximum: 255 }
+
+  validates :login_type, presence: true
+
+  enum login_type: { sorcery: 0, google: 1 }
 
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [200, 200]
