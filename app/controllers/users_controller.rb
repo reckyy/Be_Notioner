4d7b26a77b14efbf13d03a3bcc8a74fd17bef7a0
recky <srreki98@gmail.com>
2023-06-12 22:03:06 +0900

    no existのアクションを削除#114

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: %i[new create show]
+  skip_before_action :require_login, only: %i[new create]
 
   def new
     @user = User.new
