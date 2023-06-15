e06a51f523d4387dbf053c6ba234154c6c5cb2d9
recky <srreki98@gmail.com>
2023-06-15 18:44:08 +0900

    投稿機能作成#119

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,7 +1,26 @@
 class TemplatesController < ApplicationController
-  skip_before_action :require_login
 
   def index
     @templates = Template.all.includes(:user).order(created_at: :desc)
+  end
+
+  def new
+    @template = Template.new
+  end
+
+  def create
+    @template = current_user.templates.build(template_params)
+    if @template.save
+      redirect_to templates_path, success: (t '.success')
+    else
+      flash.now[:danger] = (t '.fail')
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def template_params
+    params.require(:template).permit(:name, :url)
   end
 end
