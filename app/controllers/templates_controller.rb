491a37a8bcef83421c8213a4769f24eb3c11ad50
recky <srreki98@gmail.com>
2023-06-15 16:26:43 +0900

    templateのindex作成#119

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,2 +1,7 @@
 class TemplatesController < ApplicationController
+  skip_before_action :require_login
+
+  def index
+    @templates = Template.all.includes(:user).order(created_at: :desc)
+  end
 end
