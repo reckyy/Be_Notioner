27060929f8a651eeb5b0182b34f83b76edacf0b2
recky <srreki98@gmail.com>
2023-06-15 12:26:53 +0900

    template_nameは40文字以内に設定#119

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,6 +1,6 @@
 class Template < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { maximum: 40 }
   validates :url, presence: true
 end
