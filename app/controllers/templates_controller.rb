1095e15aeeb8c6cbf57a80da92ed8cd6566863a9
recky <srreki98@gmail.com>
2023-06-16 15:31:50 +0900

    template削除機能実装#119

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -10,12 +10,19 @@ class TemplatesController < ApplicationController
 
   def create
     @template = current_user.templates.build(template_params)
-    if @template.save
+    if @template.save!
       redirect_to templates_path, success: (t '.success')
     else
       flash.now[:danger] = (t '.fail')
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @template = current_user.templates.find(params[:id])
+    @template.destroy!
+    redirect_to templates_path, status: :see_other, info: (t '.template_deleted')
+
   end
 
   private
