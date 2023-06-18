4c6350b7daa275ec887edb62e5ef232b657a102a
recky <srreki98@gmail.com>
2023-06-18 12:12:07 +0900

    環境変数名を変更#125

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -2,5 +2,5 @@ class VideoController < ApplicationController
   require 'google/apis/youtube_v3'
 
   Youtube = Google::Apis::YoutubeV3::YouTubeService.new
-  Youtube.key = ENV['YOUTUBE_API_KEY']
+  Youtube.key = ENV['GOOGLE_API_KEY']
 end
