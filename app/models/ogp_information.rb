d04bd9158dbfadff2f06b75332d7e31eeb4d0508
recky <srreki98@gmail.com>
2023-06-21 11:49:27 +0900

    ogp情報のvalidation設定#127

--- app/models/ogp_information.rb
+++ app/models/ogp_information.rb
@@ -1,2 +1,10 @@
 class OgpInformation < ApplicationRecord
+  belongs_to :informable, polymorphic: true
+
+  with_options presence: true do
+    validates :title
+    validates :url
+    validates :image
+    validates :description
+  end
 end
