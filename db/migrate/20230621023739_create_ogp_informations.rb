f51156ccf0c8c5d7d8d4e8a004050448103e64cf
recky <srreki98@gmail.com>
2023-06-21 11:52:57 +0900

    ogp_information migration完了#127

--- db/migrate/20230621023739_create_ogp_informations.rb
+++ db/migrate/20230621023739_create_ogp_informations.rb
@@ -2,7 +2,15 @@ class CreateOgpInformations < ActiveRecord::Migration[7.0]
   def change
     create_table :ogp_informations do |t|
 
+      t.string :title, null: false
+      t.string :url, null: false
+      t.string :image, null: false
+      t.string :description, null: false
+      t.string :informable_type, null: false
+      t.integer :informable_id, null: false
+
       t.timestamps
     end
+    add_index :ogp_informations, [:informable_type, :informable_id]
   end
 end
