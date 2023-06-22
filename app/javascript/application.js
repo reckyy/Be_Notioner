0ecc1c893aa8153fc5cd897ddf097528f14cd76a
recky <srreki98@gmail.com>
2023-06-23 08:15:43 +0900

    flowbiteを読み込むように追加追加#127

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-// app/javascript/packs/application.js
-$(document).ready(function(){
-  $("#popular-button").click(function(){
-    $.ajax({
-      url: "/popular",
-      type: "GET",
-      dataType: "script"
-    });
-  });
-});
+import 'flowbite'
