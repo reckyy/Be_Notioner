29bac24e55568183a0a47b60e9bc98cd47588bef
recky <srreki98@gmail.com>
2023-06-23 08:13:19 +0900

    indexアクションのみに調整#127

--- app/controllers/qiita_articles_controller.rb
+++ app/controllers/qiita_articles_controller.rb
@@ -1,10 +1,6 @@
 class QiitaArticlesController < ApplicationController
   def index
     @popular_articles = QiitaArticle.order(likes_count: :desc).limit(20)
-    respond_to do |format|
-      format.html
-      format.js
-    end
-    @popular_articles = QiitaArticle.where("created_at >= ?", 1.week.ago).order(likes_count: :desc).limit(20)
+    @recent_articles = QiitaArticle.order(created_at_qiita: :desc).limit(20)
   end
 end
