87a750932de5b3dbb696f801c579dd2b441ff5c6
recky <srreki98@gmail.com>
2023-06-23 17:36:57 +0900

    indexアクション実装#131

--- app/controllers/zenn_articles_controller.rb
+++ app/controllers/zenn_articles_controller.rb
@@ -1,4 +1,6 @@
 class ZennArticlesController < ApplicationController
   def index
+    @latest_articles = ZennArticle.where(order_type: 'latest')
+    @alltime_articles = ZennArticle.where(order_type: 'alltime')
   end
 end
