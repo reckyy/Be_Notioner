d04b0d4e62640099d910741eb41127e84d4aac17
recky <srreki98@gmail.com>
2023-06-23 17:32:01 +0900

    zenn_articleモデルにメソッド追加#131

--- app/models/zenn_article.rb
+++ app/models/zenn_article.rb
@@ -2,6 +2,49 @@ class ZennArticle < ApplicationRecord
   validates :title, presence: true
   validates :url, presence: true, uniqueness: true
   validates :likes_count
+  validates :order_type, presence: true
 
   has_one :ogp_information, as: :informable, dependent: :destroy
+
+  require 'nokogiri'
+  require 'open-uri'
+
+  TOPIC_URLS = {
+    "alltime" => "https://zenn.dev/topics/notion?order=alltime",
+    "latest" => "https://zenn.dev/topics/notion?order=latest"
+  }
+
+  def self.fetch_and_save_articles!
+    TOPIC_URLS.each do |order_type, topic_url|
+      topic_doc = Nokogiri::HTML(open(topic_url))
+      articles = topic_doc.css('.ArticleList_itemLink').take(15)
+
+      articles.each do |article|
+        title = article.at_css('.ArticleList_title').text.strip
+        url = "https://zenn.dev#{article['href']}"
+        likes_count_raw = article.at_css('.ArticleList_like')
+        likes_count = likes_count_raw.nil? ? 0 : likes_count_raw.text.strip.gsub(/\D/, '')
+
+        zenn_article = find_or_initialize_by(url: url) do |za|
+          za.title = title
+          za.likes_count = likes_count
+          za.order_type = order_type
+        end
+        zenn_article.save!
+        zenn_article.fetch_and_save_ogp_info!
+      end
+    end
+  end
+
+  def fetch_and_save_ogp_info!
+    doc = Nokogiri::HTML(open(url))
+    ogp_info = build_ogp_information unless ogp_information
+    ogp_info.title = doc.xpath('/html/head/meta[@property="og:title"]/@content').to_s
+    ogp_info.url = doc.xpath('/html/head/meta[@property="og:url"]/@content').to_s
+    ogp_info.image = doc.xpath('/html/head/meta[@property="og:image"]/@content').to_s
+    ogp_info.description = doc.xpath('/html/head/meta[@property="og:description"]/@content').to_s
+    ogp_info.save!
+  end
+end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_075007) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_082849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -113,6 +113,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_075007) do
     t.integer "likes_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "order_type", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
