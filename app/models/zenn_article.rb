a73b23b52cec047260c496e67280149daa3ae7d6
recky <srreki98@gmail.com>
2023-06-23 17:25:34 +0900

    zenn_articleとogp_informationにポリモーフィック関連設定#131

--- app/models/zenn_article.rb
+++ app/models/zenn_article.rb
@@ -2,4 +2,6 @@ class ZennArticle < ApplicationRecord
   validates :title, presence: true
   validates :url, presence: true, uniqueness: true
   validates :likes_count
+
+  has_one :ogp_information, as: :informable, dependent: :destroy
 end
