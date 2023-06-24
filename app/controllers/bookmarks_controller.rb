05d566610d8c22a50d1770c1243dd2fd6f1766f4
recky <srreki98@gmail.com>
2023-06-24 11:59:39 +0900

    bookmarks_controller実装#133

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class BookmarksController < ApplicationController
   before_action :require_login
+  before_action :set_bookmarkable
 
   def index
     @user = User.find(params[:user_id])
@@ -7,19 +8,30 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    @bookmark = current_user.bookmarks.build(bookmark_params)
-    if @bookmark.save
+    if current_user.bookmark(@bookmarkable)
       redirect_back fallback_location: root_path, success: (t '.success')
+    else
+      flash.now[:danger] = (t '.danger')
+      redirect_back fallback_location: root_path, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
-    @bookmark.destroy
+    current_user.unbookmark(@bookmarkable)
     redirect_back fallback_location: root_path, status: :see_other, success: (t '.success')
   end
 
   private
+
+  def set_bookmarkable
+    @bookmarkable = if params[:bookmarkable_type] == 'QiitaArticle'
+                      QiitaArticle.find(params[:bookmarkable_id])
+                    elsif params[:bookmarkable_type] == 'ZennArticle'
+                      ZennArticle.find(params[:bookmarkable_id])
+                    else
+                      # その他のブックマーク可能なオブジェクトを追加
+                    end
+  end
 
   def bookmark_params
     params.require(:bookmark).permit(:bookmarkable_type, :bookmarkable_id)
