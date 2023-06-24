c027281e007af8cb376f21d36c65575e0bc46472
recky <srreki98@gmail.com>
2023-06-24 10:52:37 +0900

    bookmarkモデルmigrate#133

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_082849) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_24_013052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_082849) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "bookmarkable_type", null: false
+    t.bigint "bookmarkable_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bookmarkable_type", "bookmarkable_id"], name: "index_bookmarks_on_bookmarkable"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "ogp_informations", force: :cascade do |t|
@@ -118,5 +128,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_082849) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookmarks", "users"
   add_foreign_key "templates", "users"
 end
