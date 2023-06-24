65003e144a4389b8f988ee7ad14b158bd660e3f6
recky <srreki98@gmail.com>
2023-06-24 11:21:43 +0900

    bookmarks_controller実装#133

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,27 @@
 class BookmarksController < ApplicationController
+  before_action :require_login
+
   def index
+    @user = User.find(params[:user_id])
+    @bookmarks = @user.bookmarks
   end
 
   def create
+    @bookmark = current_user.bookmarks.build(bookmark_params)
+    if @bookmark.save
+      redirect_back fallback_location: root_path, success: (t '.success')
+    end
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_back fallback_location: root_path, status: :see_other, success: (t '.success')
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:bookmarkable_type, :bookmarkable_id)
   end
 end
